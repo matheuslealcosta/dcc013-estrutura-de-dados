e55ba85c75f91a344058314e102c2763abab739d
ggiacomopz <ggiacomopz@gmail.com>
2022-10-17 17:07:06 -0300

    2

---
+++ ponteiros-alocacao/ex1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, world!" << endl;
+    int fechar = 0;
+    cout << "Deseja finalizar a execução? Digite 1" << endl;
+    cin >> fechar;
+    if (fechar = 1)
+    {
+        return 0;
+    }
+}
