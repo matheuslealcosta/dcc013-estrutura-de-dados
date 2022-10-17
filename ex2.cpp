e662cd05d2c202f0a49fa859b01a1ed518a861d8
ggiacomopz <ggiacomopz@gmail.com>
2022-10-17 17:26:11 -0300

    4

---
+++ ex2.cpp
@@ -0,0 +1,39 @@
+/*
+1.Faça um programa que realize as seguintes operaçoes:
+I Declare um ponteiro para inteiro pt.
+I Imprima o endereço de pt.
+I Crie uma variavel inteira x.
+I Leia um valor para x.
+I Faça com que pt aponte para x.
+I Imprima o conteudo da variavel pt.
+I Imprima o endereço de x.
+I Usando apenas o ponteiro pt multiplique x por 10 e altere
+o seu valor.
+I Imprima o conteudo apontado por pt.
+I Some 10 a variavel pt.
+I Imprima o seu conteudo novamente. Qual o significado
+desta saida?
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    int *pt;
+    cout << pt << endl;
+    int x;
+
+
+
+
+
+
+    int fechar = 0;
+    cout << "Deseja finalizar o programa? Digite 1" << endl;
+    cin >> fechar;
+    if (fechar = 1)
+    {
+        return 0;
+    }
+}
