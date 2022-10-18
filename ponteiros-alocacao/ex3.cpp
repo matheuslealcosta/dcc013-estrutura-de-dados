4a683bd6fd5592f6890eeb66acc4a45241ce02f4
ggiacomopz <ggiacomopz@gmail.com>
2022-10-18 15:01:50 -0300

    10

--- ponteiros-alocacao/ex3.cpp
+++ ponteiros-alocacao/ex3.cpp
@@ -9,9 +9,26 @@ void troca(int *a, int *b);
 
 using namespace std;
 
+
+void troca(int *a, int *b)
+{
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+    cout << "A: " << *a << endl << "B: " << *b << endl;
+}
+
 int main()
 {
-    
+    // define e recebe as variaveis
+    int a = 0, b = 0;
+    cout << "Digite valores inteiros para A e B" << endl;
+    cin >> a >> b;
+    cout << "A: " << a << endl << "B: " << b << endl;
+    cout << "*Trocando*" << endl;
+
+    // chama a função de troca (a função de troca imprime elas já trocadas)
+    troca(&a, &b);
 
     int fechar = 0;
     cout << endl;
